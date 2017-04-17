5bbfb01c24bbfe21463bbe9e37d7d8d63474ff26
Benny Neugebauer <bn@bennyn.de>
2017-04-18 00:17:46 +0200

    Tried to webpack scripts

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const pkg = require('./package.json');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    filename: `${__dirname}/${pkg.main}`,
+  },
+  output: {
+    filename: `${pkg.name}.js`,
+    library: pkg.name,
+    libraryTarget: 'var',
+    path: `${__dirname}/dist/window`,
+  },
+  plugins: [
+    new webpack.BannerPlugin(`${pkg.name} v${pkg.version}`),
+  ],
+  performance: {
+    maxAssetSize: 100,
+    maxEntrypointSize: 300,
+    hints: 'warning',
+  },
+};
